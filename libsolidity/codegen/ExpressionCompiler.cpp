b7a079bce7dfdf72969658f366504f35d78b02b8
chriseth <chris@ethereum.org>
2017-12-13 17:23:37 +0100

    Simple size check for old ABI decoder.

--- libsolidity/codegen/ExpressionCompiler.cpp
+++ libsolidity/codegen/ExpressionCompiler.cpp
@@ -1817,6 +1817,7 @@ void ExpressionCompiler::appendExternalFunctionCall(
 				newMem := add(mem, and(add(returndatasize(), 31), not(31)))
 			})", {"mem", "newMem"});
 			utils().storeFreeMemoryPointer();
+			m_context << Instruction::RETURNDATASIZE;
 			utils().abiDecode(_functionType.returnParameterTypes(), true);
 		}
 		else
